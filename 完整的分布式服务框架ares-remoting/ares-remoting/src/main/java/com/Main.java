02966e86dfe49c1a54793d7af221dda5eebf9364
qqwyy <qqwyy@163.com>
2019-04-21 22:50:03 +0800

    init

---
+++ 完整的分布式服务框架ares-remoting/ares-remoting/src/main/java/com/Main.java
@@ -0,0 +1,24 @@
+package com;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    /**
+     * 实例参考
+     * https://blog.csdn.net/dingqinghu/article/details/46758671
+     */
+    public static void main(String[] args) throws Exception {
+        //引入远程服务
+        final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("people.xml");
+        //获取远程服务
+//        final HelloService helloService = (HelloService) context.getBean("remoteHelloService");
+
+        People p = (People)context.getBean("people1");
+//        System.out.println(p.getId());
+        System.out.println(p.getName());
+        System.out.println(p.getAge());
+    }
+
+
+}
---
+++ 完整的分布式服务框架ares-remoting/ares-remoting/src/main/java/com/MyNamespaceHandler.java
@@ -0,0 +1,10 @@
+package com;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class MyNamespaceHandler extends NamespaceHandlerSupport {
+    @Override
+    public void init() {
+        registerBeanDefinitionParser("people", new PeopleBeanDefinitionParser());
+    }
+}
